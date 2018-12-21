94212484b4e02eda97888179bd32a3135d35c744
maple183 <39685103+maple183@users.noreply.github.com>
2018-12-21 09:38:15 +0800

    Update p13.c

--- p13.c
+++ p13.c
@@ -4,31 +4,7 @@
 
 void main()
 {
-	/*
-	//p13Á·Ï° 1-8   
-	int c;
-	int i = 0, j = 0, k = 0;
-	printf("ÇëÊäÈëÒ»¸ö×Ö·û£º\n");
-	while ((c = getchar()) != EOF)
-	{
-		
-		if (c == '\n')
-		{
-			i++;
-		}
-		if (c == '\t')
-		{
-			j++;
-		}
-		if (c == ' ')
-		{
-			k++;
-		}
-		printf("%d\t%d\t%d\n", i, j, k);
-	}
-	*/
-
-	//p13Á·Ï° 1-9  
+	//p13ç»ƒä¹  1-9  
 	int c, lastc;
 	lastc = NONBLANK;
 	while ((c = getchar()) != EOF)
